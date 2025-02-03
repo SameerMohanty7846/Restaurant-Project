12073a92a96fcc5204b3d8e171e3e9cba4a417f1
SameerMohanty7846 <sameermohanty6369@gmail.com>
2025-02-04 00:17:42 +0530

    updated

--- src/main/java/in/myorg/controller/MainController.java
+++ src/main/java/in/myorg/controller/MainController.java
@@ -5,8 +5,44 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-	@GetMapping("/")
-	public String loadPage() {
+	@GetMapping(path = { "", "welcome", "home" })
+	public String loadWelcomePage() {
 		return "index";
+	}
+	
+
+	@GetMapping("about")
+	public String loadAboutPage() {
+		return "about";
+	}
+
+	@GetMapping("booking")
+	public String loadBookingPage() {
+		return "booking";
+	}
+
+	@GetMapping("contact")
+	public String loadContactPage() {
+		return "contact";
+	}
+
+	@GetMapping("menu")
+	public String loadMenuPage() {
+		return "menu";
+	}
+
+	@GetMapping("service")
+	public String loadServicePage() {
+		return "service";
+	}
+
+	@GetMapping("team")
+	public String loadTeamPage() {
+		return "team";
+	}
+
+	@GetMapping("testimonial")
+	public String loadTestimonialPage() {
+		return "testimonial";
 	}
 }
